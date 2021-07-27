5247427c1759c7ac0f51b2384cf3f92ccd3b3875
sanif8053 <sanif.naushad98@gmail.com>
2021-07-27 15:26:24 +0500

    initial commits

---
+++ index.sh
@@ -0,0 +1,9 @@
+set -ex
+
+# echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+echo "build path "${build_path}""
+echo "token ${subscription_key}"
+
+curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+--header "SubscriptionKey: ${subscription_key}" \
+--form "applicationFile=@"${build_path}""
