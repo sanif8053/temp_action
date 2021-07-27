e588ff5c2ca31595088966a7c7186498d1094ecd
sanif8053 <48414031+sanif8053@users.noreply.github.com>
2021-07-27 18:37:44 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,10 +1,10 @@
 set -ex
-
+echo "hello"
 # echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-echo "build path "${build_path}""
-echo "token ${subscription_key}"
+# echo "build path "${build_path}""
+# echo "token ${subscription_key}"
 
-response = curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
---header "SubscriptionKey: ${subscription_key}" \
---form "applicationFile=@"${build_path}""
-echo ${response}
+# response = curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+# --header "SubscriptionKey: ${subscription_key}" \
+# --form "applicationFile=@"${build_path}""
+# echo ${response}
