2ddc865ebe2c2188590c1cf3fdd8bdd49c3c070b
sanif8053 <48414031+sanif8053@users.noreply.github.com>
2021-07-27 17:51:38 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -4,6 +4,7 @@ set -ex
 echo "build path "${build_path}""
 echo "token ${subscription_key}"
 
-curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+response = curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
 --header "SubscriptionKey: ${subscription_key}" \
 --form "applicationFile=@"${build_path}""
+echo ${response}
