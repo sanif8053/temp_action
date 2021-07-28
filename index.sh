d4659f13ddbd7998521a7810afb902cc6670dc54
sanif8053 <48414031+sanif8053@users.noreply.github.com>
2021-07-28 15:44:31 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@ echo "hello"
 # echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 echo "build path "${build_path}""
 echo "token ${subscription_key}"
-echo "${GITHUB_WORKSPACE}" 
+echo "${GITHUB_ACTION_PATH}" 
 
 
 
