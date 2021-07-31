9ec4f89c57c08e606b7c14b67cf133ee8036122e
sanif8053 <48414031+sanif8053@users.noreply.github.com>
2021-07-31 14:59:58 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@ echo "hello"
 # echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 echo "build path "${build_path}""
 echo "token ${subscription_key}"
-search_dir="./"
+search_dir="/"
 
 
 for entry in "$search_dir"/*
